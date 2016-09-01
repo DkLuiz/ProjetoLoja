51f325e7a3d2c8d5ca28da4e032a09080a0d16ff
Flavio Moreira <flaviorsm@gmail.com>
2016-09-01 19:51:41 -0300

    Update LojaFisico.sql

--- LojaFisico.sql
+++ LojaFisico.sql
@@ -6,22 +6,22 @@ CREATE DATABASE PLOJA;
 CREATE TABLE ItensPedido (
 IdProduto INT,
 NumeroPedido INT,
-Quantidade INT,
-PRIMARY KEY(IdProduto,NumeroPedido)
+Quantidade INT
 );
 
 CREATE TABLE Cliente (
-IdCliente INT PRIMARY KEY,
+IdCliente INT AUTO_INCREMENT,
 Nome VARCHAR(65),
 CPF VARCHAR(11),
 DataNascimento DATE,
 Sexo CHAR(1),
 Email VARCHAR(80),
-Senha VARCHAR(20)
+Senha VARCHAR(20),
+PRIMARY KEY(IdCliente)
 );
 
 CREATE TABLE Endereco (
-IdEndereco INT PRIMARY KEY,
+IdEndereco INT AUTO_INCREMENT,
 IdCliente INT,
 CEP VARCHAR(8),
 Endereco VARCHAR(200),
@@ -31,43 +31,48 @@ Bairro VARCHAR(50),
 Cidade VARCHAR(50),
 UF CHAR(2),
 Ativo CHAR(1),
+PRIMARY KEY(IdEndereco),
 FOREIGN KEY(IdCliente) REFERENCES Cliente (IdCliente)
 );
 
 CREATE TABLE Preco (
-IdPreco INT PRIMARY KEY,
+IdPreco INT AUTO_INCREMENT,
 IdProduto INT,
 Data DATE,
 TipoPreco VARCHAR(50),
 Preco FLOAT,
-Ativo CHAR(1)
+Ativo CHAR(1),
+PRIMARY KEY(IdPreco)
 );
 
 CREATE TABLE Produto (
-IdProduto INT PRIMARY KEY,
-NomeProduto VARCHAR(50)
+IdProduto INT AUTO_INCREMENT,
+NomeProduto VARCHAR(50),
+PRIMARY KEY(IdProduto)
 );
 
 CREATE TABLE Pedido (
-IdPedido INT PRIMARY KEY,
+IdPedido INT AUTO_INCREMENT,
 NumeroPedido INT NOT NULL,
 IdCliente INT,
 DataPedido DATE,
 DataEnvio DATE,
 Frete FLOAT,
 PrecoPedido FLOAT,
+PRIMARY KEY(IdPedido),
 FOREIGN KEY(IdCliente) REFERENCES Cliente (IdCliente)
 );
 
 CREATE TABLE Telefone (
-IdTelefone INT PRIMARY KEY,
+IdTelefone INT AUTO_INCREMENT,
 IdCliente INT,
 Telefone VARCHAR(14),
 Tipo VARCHAR(20),
 Ativo CHAR(1),
+PRIMARY KEY(IdTelefone),
 FOREIGN KEY(IdCliente) REFERENCES Cliente (IdCliente)
 );
 
-ALTER TABLE ItensPedido ADD FOREIGN KEY(IdProduto) REFERENCES Produto (IdProduto);
-ALTER TABLE ItensPedido ADD FOREIGN KEY(NumeroPedido) REFERENCES Pedido (NumeroPedido);
+ALTER TABLE ItensPedido ADD CONSTRAINT FK_PRODUTO_IDPRODUTO FOREIGN KEY(IdProduto) REFERENCES Produto (IdProduto);
+ALTER TABLE ItensPedido ADD CONSTRAINT FK_PEDIDO_IDPEDIDO FOREIGN KEY(IdPedido)    REFERENCES Pedido  (IdPedido);
 ALTER TABLE Preco ADD FOREIGN KEY(IdProduto) REFERENCES Produto (IdProduto);
